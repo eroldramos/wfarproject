e6f1adeb7532d821c5dadca848f826f98889136e
erikasraymundo <erika.raymundo.s@bulsu.edu.ph>
2022-04-20 05:42:34 +0800

    Models and database structure
    Regarding sa User and Faculty, gumawa muna ako ng model na faculty, i-lipat na lang natin 'yung fields sa User kung hindi pala applicable 'yung nagawa ko. Thanks!

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-
-from core.models import Faculty
-from core.models import User
+from core.models import Faculty, Semester, User, Week, WFAR, WFAR_Entry, WFAR_Entry_Activity, WFAR_Entry_Attachment, WFAR_Comment
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Faculty)
+admin.site.register(Semester)
+admin.site.register(Week)
+admin.site.register(WFAR)
+admin.site.register(WFAR_Entry)
+admin.site.register(WFAR_Entry_Activity)
+admin.site.register(WFAR_Entry_Attachment)
+admin.site.register(WFAR_Comment)
--- backend/core/models.py
+++ backend/core/models.py
@@ -1,5 +1,4 @@
-from statistics import mode
-from tkinter.tix import Tree
+from datetime import datetime
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
@@ -16,6 +15,10 @@ class User(AbstractUser):
     def __str__(self):
         return f"{self.email}"
 
+# Kung saka-sakali na si Faculty is User na lang din, palipat na lang nung fields ni Faculty kay user hehe.
+# Tapos, lahat ng Faculty na foreign key pakigawa na lang na User 
+# faculty_id => user_id
+
 # Faculty
 class Faculty(models.Model):
     user_type = models.PositiveSmallIntegerField(default=1) # determines if normal faculty, area chair or dept head
@@ -27,16 +30,16 @@ class Faculty(models.Model):
     mname = models.CharField(max_length=200, null=True)
     lname = models.CharField(max_length=200)
     ext_name = models.CharField(max_length=200, null=True)
-    birthdate = models.DateField
-    civil_status = models.PositiveSmallIntegerField
-    sex = models.PositiveSmallIntegerField
-    house_no = models.PositiveIntegerField
+    birthdate = models.DateField()
+    civil_status = models.PositiveSmallIntegerField()
+    sex = models.PositiveSmallIntegerField()
+    house_no = models.PositiveIntegerField()
     street = models.CharField(max_length=200)
     subdivision = models.CharField(max_length=200, null=True)
     barangay = models.CharField(max_length=200)
     municipality = models.CharField(max_length=200)
     province = models.CharField(max_length=200)
-    zip_code = models.PositiveIntegerField
+    zip_code = models.PositiveIntegerField()
     contact_no = models.CharField(max_length=20, unique=True)
     specialization = models.CharField(max_length=200, null=True)
     program = models.CharField(max_length=200, null=True)
@@ -47,15 +50,48 @@ class Faculty(models.Model):
 
 # Semester
 class Semester(models.Model):
-    label = models.CharField(200)
-    school_year = models.CharField(200)
+    label = models.CharField(max_length=200)
+    school_year = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     deleted_at = models.DateTimeField(null=True)
 
 # Week
 class Week(models.Model):
-    label = models.CharField(200)
-    start_date = models.DateTimeField
-    end_date = models.DateTimeField
+    label = models.CharField(max_length=200)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
     semester_id = models.ForeignKey(Semester, on_delete=models.CASCADE) # fk for semester
+
+# WFAR
+class WFAR(models.Model):
+    status = models.PositiveSmallIntegerField(default=1) # 1 - not checked, 2 - ok, 3 - with revisions
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    faculty_id = models.ForeignKey(Faculty, on_delete=models.CASCADE) #fk for faculty
+
+# WFAR Entry
+class WFAR_Entry(models.Model):
+    accomplishment_date = models.DateTimeField(default=datetime.now)
+    subject = models.CharField(max_length=200)
+    course_year_section = models.CharField(max_length=200)
+    no_of_attendees = models.PositiveIntegerField()
+    recording_url = models.TextField(max_length=500)
+    wfar_id = models.ForeignKey(WFAR, on_delete=models.CASCADE)
+
+# WFAR Entry Attachment
+class WFAR_Entry_Attachment(models.Model):
+    image_uri = models.ImageField(upload_to='uploads/') # image, paki-palitan na lang 'yung uploads/
+    type = models.PositiveSmallIntegerField(default=1) # 1 - meet sc, 2 - activity
+    wfar_entry_id = models.ForeignKey(WFAR_Entry, on_delete=models.CASCADE)
+
+# WFAR Entry Learning Activities
+class WFAR_Entry_Activity(models.Model):
+    description = models.TextField(max_length=500)
+    wfar_entry_id = models.ForeignKey(WFAR_Entry, on_delete=models.CASCADE)
+
+# WFAR Comment
+class WFAR_Comment(models.Model):
+    description = models.TextField(max_length=1000)
+    wfar_id = models.ForeignKey(WFAR, on_delete=models.CASCADE)
+    faculty_id = models.ForeignKey(Faculty, on_delete=models.CASCADE)
