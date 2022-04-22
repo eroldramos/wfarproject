d26de9c25661d9e3784cfcd251b50446d7871bfd
Erold Ramos <dev.eroldramos@gmail.com>
2022-04-22 21:20:42 +0800

    changed html attrs to jsx readable attrs

--- frontend/src/components/UI/TextField.js
+++ frontend/src/components/UI/TextField.js
@@ -5,7 +5,7 @@ import './TextField.css';
 const TextField = (props) => {
     return (
         <Fragment>
-            <label for={props.id}>{props.label}</label>
+            <label htmlFor={props.id}>{props.label}</label>
             <input type="text" id={props.id} name={props.name} placeholder={props.placeholder}/>
         </Fragment>
 
