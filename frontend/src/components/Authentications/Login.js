02fa81bf8d7e4e5da4477176e144acb1761448d0
Erold Ramos <dev.eroldramos@gmail.com>
2022-04-26 23:51:06 +0800

    new libraries in package.json

--- frontend/src/components/Authentications/Login.js
+++ frontend/src/components/Authentications/Login.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from "react";
-import TextField from "../UI/TextField";
+import TextField from "../UI/FormControl/InputField/TextField";
 import classes from "./Login.module.css";
 const Login = () => {
   return (
