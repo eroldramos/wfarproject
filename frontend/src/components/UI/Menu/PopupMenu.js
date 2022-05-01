6a7833a3067ecf3bd32d059dd6f69ece4954f41a
erikasraymundo <erika.raymundo.s@bulsu.edu.ph>
2022-05-02 02:03:05 +0800

    my submission + popup menu component

---
+++ frontend/src/components/UI/Menu/PopupMenu.js
@@ -0,0 +1,22 @@
+import styles from './PopupMenu.module.css';
+
+const PopupMenu = (props) => {
+
+    const items = props.items;
+
+    return (
+        <div className={styles['pop-up']}>
+            <ul>
+                {items.map(item => {
+                    return (
+                        <li key={item.id} onClick={item.onClick}>
+                            {item.label}
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
+    );
+}
+
+export default PopupMenu;
