51b9169f3279d2717c53b1ddf0f612ff255c4dce
Erold Ramos <dev.eroldramos@gmail.com>
2022-05-03 14:55:51 +0800

    merge

---
+++ frontend/src/components/Dashboard/Dashboard-components/WFARunchecked.js
@@ -0,0 +1,19 @@
+const WFARunchecked = (props) => {
+
+    return (
+        <div className="wfar-unchecked">
+            <div className="info">
+                <h3>WFAR Unchecked</h3>
+                <p className="week">Week 7</p>
+            </div>
+            <div className="number-unchecked-container">
+                <div className="vertical-design"></div>
+                <div className="number-unchecked">
+                    <p>02</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default WFARunchecked;
---
+++ frontend/src/components/WfarForm/Attachments/TeamMeetScreenshots.js
@@ -0,0 +1,13 @@
+import styles from './LearningActivities.module.css';
+import DynamicInputField from '../UI/DynamicInputField';
+import { useState } from 'react';
+const TeamMeetScreenshots = (props) => {
+
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default TeamMeetScreenshots;
