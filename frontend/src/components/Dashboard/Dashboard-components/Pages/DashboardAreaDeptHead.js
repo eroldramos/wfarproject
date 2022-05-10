8fa61488cf68d8cb1542e3499e4db9883fb61465
sheenpai <86642979+sheenpai@users.noreply.github.com>
2022-05-10 19:27:53 +0800

    all dashboard FE done

---
+++ frontend/src/components/Dashboard/Dashboard-components/Pages/DashboardAreaDeptHead.js
@@ -0,0 +1,69 @@
+import "../../DashboardCSS/Dashboard-area-depthead.css"
+import pendingAccountIcon from "../../../../assets/DashboardDummyImg/access_time.svg"
+import NotificationInDashboard from "../../Dashboard-components/NotificationInDashboard";
+
+
+import WFARreceived from "../../Dashboard-components/WFARreceived";
+import WFARunchecked from "../../Dashboard-components/WFARunchecked";
+import ManageFacultiesUsers from "../../Dashboard-components/ManagementFacultiesUser";
+import WFARstatusDashboard from "../../Dashboard-components/WFARstatusDashboard";
+import WFARStatus from "../../../UI/WFAR/WFARStatus/WFARStatus";
+
+const DashboardAreaDeptHead = () => {
+
+    return (
+        <div className="dashboard-area-depthead">
+
+            <div className="main-container">
+                <div className="header">
+                    <h1>Dashboard</h1>
+                </div>
+
+                <NotificationInDashboard/>
+
+                <div className="total-submission-container">
+                    <WFARreceived/>
+                    <WFARunchecked/>
+                </div>
+
+                <WFARstatusDashboard/>
+
+                <div className="my-wfar">
+                    <div className="my-wfar-info">
+                        <h3>My WFAR</h3>
+                        <p>View All</p>
+                    </div>
+                    <div>
+                        <WFARStatus/>
+                    </div>
+                </div>
+
+                <div className="management-container">
+                    <div className="faculties">
+                        <h3>Faculties</h3>
+                    </div>
+                    <div className="managed-faculties">
+                        <ManageFacultiesUsers/>
+                        <ManageFacultiesUsers/>
+                        <ManageFacultiesUsers/>
+                        <ManageFacultiesUsers/>
+                        <ManageFacultiesUsers/>
+                        <ManageFacultiesUsers/>
+                        <ManageFacultiesUsers/>
+                    </div>
+
+                    <div className="manage-pending-accounts">
+                        <div className="pending-accounts-icon">
+                            <img src={pendingAccountIcon} alt="" />
+                        </div>
+                        <h3>Pending Accounts</h3>
+                    </div>
+                </div>
+            </div>
+            
+            
+        </div>
+    );
+};
+
+export default DashboardAreaDeptHead;
