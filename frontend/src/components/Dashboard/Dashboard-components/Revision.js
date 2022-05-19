85d79da73c375b55619adce259116ff7862fbf4c
sheenpai <86642979+sheenpai@users.noreply.github.com>
2022-05-20 04:15:14 +0800

    routing revision

--- frontend/src/components/Dashboard/Dashboard-components/Revision.js
+++ frontend/src/components/Dashboard/Dashboard-components/Revision.js
@@ -1,6 +1,7 @@
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { getAllWFARComments } from "../../../store/dashboardAction";
+import { Link } from "react-router-dom";
 
 const Revision = (props) => {
 
@@ -13,7 +14,7 @@ const Revision = (props) => {
     const comment = useSelector((state) => state.allWFARcomments).comment;
     let outputComement = " ";
     let diffDays = 0;
-    if (comment.length>0) {
+    if (comment.length > 0) {
         outputComement = comment[0].description;
         let today = new Date();
         let date = today.getFullYear() + ',' + (today.getMonth() + 1) + ',' + today.getDate();
@@ -21,8 +22,8 @@ const Revision = (props) => {
         const date2 = new Date(comment[0].created_at_date);
         const diffTime = Math.abs(date2 - date1);
         diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-        if(diffTime<57600000){
-            diffDays=-1;
+        if (diffTime < 57600000) {
+            diffDays = -1;
         }
     }
 
@@ -30,11 +31,14 @@ const Revision = (props) => {
         <div className="revision-instance">
             <div className="revision-date">
                 <div className="revision-week">Week {props.revision.week_no}</div>
-                <div className="revision-day">Commented {diffDays<1?'today': diffDays+ (diffDays==1?' day ago':' days ago') } </div>
+                <div className="revision-day">Commented {diffDays < 1 ? 'today' : diffDays + (diffDays == 1 ? ' day ago' : ' days ago')} </div>
             </div>
             <div className="revision-comment">{outputComement}</div>
             <div className="edit-button">
-                <p>Edit</p>
+                <Link to={'/mySubmission/'}>
+                    <p>Edit</p>
+                </Link>
+                
             </div>
         </div>
     )
