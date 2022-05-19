4ba53111f3d429fd4ea32f382cb2d5d506aac165
kenetghell21 <kenetghellb@gmail.com>
2022-05-20 05:42:03 +0800

    profile finishing

--- frontend/src/components/Screens/AccountScreen.js
+++ frontend/src/components/Screens/AccountScreen.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from "react";
-import Profile from "../Account/Profile2ComponentBase.js";
+import Profile from "../Account/UserProfile";
 const AccountScreen = () => {
   return (
     <Fragment>
